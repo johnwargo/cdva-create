d6a68f73d5779f33f1a62729216bf3fe8adbe285
John M. Wargo <john@johnwargo.com>
2014-12-12 08:08:45 -0800

    Initial load
    This is a rename of the Cordova-create project just to avoid name
    conflicts with anything the Cordova project does.

--- README.md
+++ README.md
@@ -1,4 +1,117 @@
-cdva-create
-===========
+Cordova Create (cdva-create)
+============================
 
-A node module for creating a simple Cordova project with a couple of common plugins and platforms. Module is configurable via a configuration file.
+A module for creating a simple Cordova project with a couple of common platforms and plugins added. Essentially, this command replaces the common set of commands a Cordova developer types every (and I do mean every) time he or she creates a new Cordova project. 
+
+If you create an occasional Cordova application, this probably won't be that useful to you. For me, in my day job as a product manager for a set of enterprise plugins for Apache Cordova and my side job, which is producing a catalog of books on Apache Cordova, I find that I'm constantly creating new Cordova projects. This module should save you a little time and make it very easy to create new Cordova projects.
+
+The module is customizable; when you run the command the first time, it creates a configuration file that you can easily modify to customize how the module works for you. Through the configuration file, you can specify the platforms and plugins that are added to the project created using the command. You can even enable debug mode, which causes the module to execute the `cordova` command with it's debug (-d) command line option. All of the customization capabilities will be described later.
+
+Requirements
+============
+This module expects that you have a functional Apache Cordova development environment running. This includes the appropriate native SDKs (Android Development Tools, Xcode and so on), NodeJS and other associated tools. As the command is a node module, it relies upon the same technology as the Cordova CLI does. 
+
+This module has been tested on Windows, Macintosh OS X and Ubuntu Linux.
+
+An excellent resource for information on how to setup a Cordova development environment is my [Apache Cordova 3 Programming](http://www.cordovaprogramming.com) book.
+
+Installation
+============
+Install this module using npm by opening a terminal window and executing the following command:
+
+Windows:
+
+	npm install -g cdva-create
+
+Macintosh OS X and Linux:
+
+	sudo npm install -g cdva-create
+
+
+If you've downloaded this module's code from GitHub, you can install the module by extracting the files then opening a terminal window and navigating to the folder where this file is located and issuing the following command:
+
+Windows:
+
+	npm install -g
+
+Macintosh OS X and Linux:
+
+	sudo npm install -g
+
+Usage
+===========
+To create a new project using this command, open a terminal window and navigate to the folder where you want the project created and issue the following command:
+
+	cdva-create folder app_id app_name [platform list]
+
+The first three parameters are the same parameters you would use with the `cordova create` command. The platform list, shown as an optional parameter (by the use of brackets), defines the list of target platforms you want to use for the application project.
+
+To create a sample Android project called Hello2 in a folder called hello_2 you would use the following command:
+
+	cdva-create hello_2 com.johnwargo.hello2 Hello2 android
+
+To create the same project, but include an iOS project as well, you would use the following command:
+
+	cdva-create hello_2 com.johnwargo.hello2 Hello2 android ios
+
+If you do not specify a platform list on the command line, the set of default platforms for your current operating system (described below) will be used.
+
+To launch the system's default editor to edit the application's configuration file (described in the next section), use the following command:
+
+	cdva-create /config 
+
+Customization
+======================
+When the command runs for the first time, it creates a configuration file that can be used to customize the tasks the command performs when it runs. The configuration file is called `cdva-create.json` and it can be located in the user's home folder. On Windows you can find the file in the c:\users\user_name folder (replacing user_name with the login name for the current user). On Macintosh OS X, the file is located in the user's home folder at /Users/user_name (again replacing user_name with the user's logon name).
+
+The default options for the application are defined in the following JSON object stored in the configuration file:
+
+    {
+      "platformList": [ "android", "firefoxos", "ios" ],
+      "pluginList": [ "org.apache.cordova.console", "org.apache.cordova.dialogs", "org.apache.cordova.device" ],
+      "enableDebug": false,
+	  "copyFrom": "folder_path"
+	  "linkTo": "folder_path"
+	  "createParms": "\"{\\\"plugin_search_path\\\":\\\"d:/dev/\\\"}\""
+    }
+
+To change the module's configuration, edit the JSON object, providing your own values for the configuration options described by the object. 
+
+The default list of target platforms will differ depending on what operating system you are using. If you look at the script's code, you will see the following default platforms lists:
+
+  	var default_platforms_linux = ['ubuntu'];
+  	var default_platforms_osx = ['android', 'ios'];
+  	var default_platforms_win = ['android', 'windows'];
+	
+You can add third party plugins to the pluginlist. This should work as long as the Cordova CLI can load the plugins using the plugin's ID. Where this won't work is for locally installed plugins. If you want to use locally installed plugins, you will need to set a plugin search path during the call to the `cordova create` command. 
+
+The enableDebug parameter causes the module to add the debug (-d) parameter to all `cordova` CLI commands. With this option set to true, additional information will be written to the console as the Cordova CLI commands are executed. You will want to enable this option if something isn't working with the command and you want more information about what's happening as the different commands are executed. 
+
+The copyFrom property allows you to specify the folder path that is used for the source for the web application content for the created application. When a non-blank value is provided here, the `cordova create` command is enhanced by adding a `--copy-from "folder_path"` to the end of the command. The folder_path can be an absolute or relative path - all that matters is that the Cordova CLI can resolve the path.
+
+With linkto, a symbolic link is created in the project's www folder that points to the specified folder path. When a non-blank value is provided here, the `cordova create` command is enhanced by adding a `--link-to "folder_path"` to the end of the command. The folder_path can be an absolute or relative path; all that matters is that the Cordova CLI can resolve the path.
+
+The createParms property defines a JSON object that is used to populate a `config.json` file located in the new project's `.cordova` folder.  When a non-blank value is provided here, the JSON object is passed as a parameter on the end of the `cordova create` command. Using the example above, this option instructs the CLI to populate the `cordova.json` file with the following content: 
+
+	{ "plugin_search_path": "d:/dev/" }
+
+With this in place, any `platform add` commands will search the specified path for plugins. The weird triple backslashes in the JSON string are there so that the content's quote and backslash characters will be properly escaped by the JSON parser. You can populate additional properties in the `config.json` file by appending the appropriate JSON content to the createParms property.
+
+Note: You can create an empty folder then enable this option (passing in the path to the empty folder) to create a Cordova project with no web application content in the application's www folder.
+
+Many people enable the option by default for all cordova commands, but this really doesn't make sense since, in a properly configured Cordova development environment, stuff just works. Don't succumb, only enable this option when it's really useful or needed. 
+
+Update History
+==============
+November 5, 2014
+----------------
+Added /config command-line option. Opens the application's configuration file in the system's default editor.  
+
+October 28, 2014
+-----------------
+Added support for the Cordova CLI create command's --link-to switch.
+
+Added support for passing additional parameters to the create command. This was added in order to support adding a plugin search path to the project's configuration when first creating the project. This was needed to support the SAP Mobile Platform Hybrid SDK since the SDK plugins are installed locally and the CLI has issues locating dependent plugins when installed locally.  
+ 
+* * *
+By [John M. Wargo](http://www.johnwargo.com) - if you like and/or use this module, why not pick up [one of my books](http://www.johnwargobooks.com)?
---
+++ cordova-create.js
@@ -0,0 +1,258 @@
+#!/usr/bin/env node
+
+//========================================================================
+// cordova-create
+//
+// A node command for creating a simple Cordova project with a couple 
+// of common plugins.
+//
+// by John M. Wargo (www.johnwargo.com)
+//========================================================================
+var appConfig = require('./app_config.js');
+var colors = require('colors');
+var exec = require('child_process').exec;
+var fs = require('fs');
+var path = require('path');
+var shelljs = require('shelljs');
+
+//*************************************
+//some constants
+//*************************************
+var commaSpace = ', ';
+var cmdStr = 'cdva-create folder app_id app_name [platform list]';
+var debug = false;
+var helpFile = 'help.txt';
+var plugin_list;
+var quoteMark = '"';
+var theStars = "***************************************";
+var space = ' ';
+
+colors.setTheme({
+  info: 'grey',
+  help: 'green',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+
+function listArray(theName, theArray) {
+  //Write the contents of an array to the console
+  console.log("\n%s Array Contents", theName);
+  for (var i = 0; i < theArray.length; i++) {
+    console.log("%s[%s]: '%s'", theName, i, theArray[i]);
+  }
+}
+
+function showHelp() {
+  //read the help file
+  var raw = fs.readFileSync(path.join(__dirname, helpFile)).toString('utf8');
+  //write the contents of the help file to the console
+  console.log(raw);
+}
+
+function executeCordovaCommand(commandStr) {
+  //Build the base command
+  var theCmd = 'cordova ';
+  //do we want to enable debug mode?
+  if (theConfig.enableDebug) {
+    //Then append the -d to the cordova command
+    console.log("Enabling debug mode");
+    theCmd += '-d ';
+  }
+  //Now add the rest of the command to the string
+  theCmd += commandStr;
+  console.log('Command string: %s', theCmd);
+  var resCode = shelljs.exec(theCmd).code;
+  if (resCode !== 0) {
+    console.error("Unable to execute command (error code: %s)".error, resCode);
+    process.exit(1);
+  }
+}
+
+//========================================================================
+//Write out what we're running
+//========================================================4================
+console.log("\n%s".help, theStars);
+console.log("*    Cordova Create (cdva-create)    *".help);
+console.log(theStars.help);
+
+//========================================================================
+//Sort out the command line arguments
+//========================================================================
+var userArgs;
+//Is the first item 'node'? then we're testing
+if (process.argv[0].toLowerCase() == 'node') {
+  //whack the first two items off of the list of arguments
+  //This removes the node entry as well as the cordova-create entry (the
+  //program we're running)
+  userArgs = process.argv.slice(2);
+} else {
+  //whack the first item off of the list of arguments
+  //This removes just the cva-create entry
+  userArgs = process.argv.slice(1);
+}
+//What's left at this point is just all of the parameters
+if (debug) {
+  listArray('Arguments', userArgs);
+}
+
+//Do we have the right # of parameters to continue?
+if (userArgs.length > 2) {
+  //Get the app's configuration settingds
+  var theConfig = appConfig.getConfig();
+  //Write them to the log while we're testing this thing...
+  //console.log('App Config: %s'.error, JSON.stringify(theConfig));
+
+  //Grab the target folder
+  var targetFolder = userArgs[0];
+  //Get the app ID
+  var appID = userArgs[1];
+  //grab the app name
+  var appName = userArgs[2];
+  //now whack off the initial (first three) arguments
+  var userArgs = userArgs.slice(3);
+  //What's left is any target platforms (if we have any)
+  var targetPlatforms = [];
+  //Do we have any platforms on the command line?
+  if (userArgs.length > 0) {
+    //Then use them
+    targetPlatforms = userArgs;
+  } else {
+    targetPlatforms = theConfig.platformList;
+  }
+
+  //========================================================================
+  //Check to make sure that the target folder does not already exist
+  //========================================================================
+  if (fs.existsSync(targetFolder)) {
+    console.error("\nTarget folder %s already exists".error, targetFolder);
+    process.exit(1);
+  }
+
+  //========================================================================
+  //Read the plugin list from the config
+  //========================================================================
+  plugin_list = theConfig.pluginList;
+
+  //========================================================================
+  //Tell the user what we're about to do
+  //========================================================================
+  console.log("\n" + theStars);
+  console.log("Application Name: %s", appName);
+  console.log("Application ID: %s", appID);
+  console.log("Target folder: %s", targetFolder);
+  console.log("Target platforms: %s", targetPlatforms.join(commaSpace));
+  console.log('Plugins: %s', plugin_list.join(commaSpace));
+  console.log(theStars);
+
+  //========================================================================
+  //create the Cordova project
+  //========================================================================
+  console.log("\nCreating project".warn);
+  console.log(theStars);
+  var cmdStr = 'create ' + targetFolder + space + appID + space + quoteMark + appName + quoteMark;
+  var copyFromPath = theConfig.copyFrom;
+  //Do we have a copyFrom path?
+  if (copyFromPath.length > 0) {
+    //Then add it to the end of the create command
+    console.log('Enabling --copy-from option (file path: %s)', copyFromPath);
+    //Then add it to the command string we're executing
+    cmdStr += ' --copy-from "' + copyFromPath + quoteMark;
+  } else {
+    //Only do the linkTo option if copyTo is blank
+    //can't have both
+    var linkToPath = theConfig.linkTo;
+    if (linkToPath.length > 0) {
+      //Then add it to the end of the create command
+      console.log('Enabling --link-to option (file path: %s)', linkToPath);
+      //Then add it to the command string we're executing
+      cmdStr += ' --link-to "' + linkToPath + quoteMark;
+    }
+  }
+
+  //Pass any additional create command paramaters that exist in the
+  //config file
+  var createParms = theConfig.createParms;
+  if (createParms.length > 0) {
+    console.log('Appending "%s" to the create command', createParms);
+    cmdStr += " " + createParms;
+  }
+  executeCordovaCommand(cmdStr);
+
+  //========================================================================
+  //Change to the target folder directory
+  //========================================================================
+  console.log("\nChanging to project folder (%s)".warn, targetFolder);
+  console.log(theStars);
+  shelljs.pushd(targetFolder);
+
+  //========================================================================
+  // Platforms
+  //========================================================================
+  console.log('\nAdding platforms [%s] to the project'.warn, targetPlatforms.join(commaSpace));
+  console.log(theStars);
+  if (targetPlatforms.length > 0) {
+    executeCordovaCommand('platform add ' + targetPlatforms.join(space));
+  } else {
+    //I guess we're not adding any platforms
+    //warn, but don't fail
+    console.log("No platforms specified, skipping".warn);
+  }
+
+  //========================================================================
+  // Plugins
+  //========================================================================
+  console.log("\nAdding Cordova Core Plugins".warn);
+  console.log(theStars);
+  if (plugin_list.length > 0) {
+    // Loop through plugins array rather than hard-coding this list
+    plugin_list.forEach(function (plugin) {
+      console.log("Adding %s plugin to project".info, plugin);
+      executeCordovaCommand('plugin add ' + plugin);
+    });
+  } else {
+    //I guess we're not adding any plugins
+    //warn, but don't fail
+    console.log("No plugins specified in the configuration file, skipping...".warn);
+  }
+
+  //========================================================================
+  // Finished
+  //========================================================================
+  console.log("\nAll done!\n".help);
+
+} else {
+  //Do we have only one parameter and it's the word 'config'?
+  if (userArgs.length == 1 && userArgs[0].toLowerCase() == '/config') {
+    //Then open the config file for editing
+    //First get the config file name
+    var configFile = appConfig.getConfigFile();
+    //Tell the user we're launching it
+    console.log("Launching %s", configFile);
+    //Figure out what command will launch the file depending on the
+    //operating system
+    var cmdStr;
+    if (appConfig.isWindows()) {
+      cmdStr = "start " + configFile;
+    } else {
+      cmdStr = "open " + configFile;
+    }
+    var child;
+    child = exec(cmdStr, function (error, stdout, stderr) {
+      if (error !== null) {
+        console.log("\nexec error: %s\n".error, error);
+        process.exit(1);
+      }
+    });
+  } else {
+    //Otherwise, we don't know what to do, so toss out an error
+    //Tell the user why we can't do anything
+    console.error("\nMissing one or more parameters, the proper command format is: ".error);
+    //Show them the offending command line
+    console.error("\n  %s\n".error, cmdStr);
+    //Then display the help file
+    showHelp();
+    //We're done, so exit the app
+    process.exit(1);
+  }
+}
